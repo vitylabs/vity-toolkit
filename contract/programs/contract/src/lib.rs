6e849b9194c7c3eb553766015e3f1b4953e95066
thatsmeadarsh <thatsmeadarshgupta@gmail.com>
2024-12-20 20:36:31 +0530

    feat: Made the smart contract to store the auth details for both app and user

--- contract/programs/contract/src/lib.rs
+++ contract/programs/contract/src/lib.rs
@@ -6,11 +6,105 @@ declare_id!("AEqfthAnEGQ27vp6Dy9nDzZPJL1wcicbGeiLcWnptn3N");
 pub mod contract {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn save_app_auth(
+        ctx: Context<SaveAppAuth>,
+        app_name: String,
+        auth_uri: String,
+    ) -> Result<()> {
+        // setting userdata in app's account
+        let app_auth_data = &mut ctx.accounts.app_auth;
+        app_auth_data.bump = ctx.bumps.app_auth;
+
+        app_auth_data.app_id = *ctx.accounts.signer.key;
+        app_auth_data.app_name = app_name.to_owned();
+        app_auth_data.auth_uri = auth_uri.to_owned();
+
+        // Printing App Info into program's on-chain transaction log.
+        msg!("Saved App Auth Data");
+        Ok(())
+    }
+
+    pub fn save_user_auth(
+        ctx: Context<SaveUserAuth>,
+        app_id: Pubkey,
+        app_name: String,
+        auth_uri: String,
+    ) -> Result<()> {
+        // setting userdata in user's account
+        let user_auth_data = &mut ctx.accounts.user_auth;
+        user_auth_data.bump = ctx.bumps.user_auth;
+
+        user_auth_data.authority = *ctx.accounts.signer.key;
+        user_auth_data.app_id = app_id.to_owned();
+        user_auth_data.app_name = app_name.to_owned();
+        user_auth_data.auth_uri = auth_uri.to_owned();
+
+        // Printing User Info into program's on-chain transaction log.
+        msg!("Saved User Auth Data");
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct SaveAppAuth<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init_if_needed,
+        payer = signer,
+        space = SaveAppAuth::LEN,
+        seeds = ["app-auth".as_bytes(), signer.key().as_ref()], 
+        bump,
+    )]
+    pub app_auth: Account<'info, AppAuth>,
+    pub system_program: Program<'info, System>,
+}
+
+impl SaveAppAuth<'_> {
+    const LEN: usize = 8 + // discriminator
+        32 + // app_id
+        32 + // app_name
+        32 + // auth_uri
+        1; // bump
+}
+
+#[derive(Accounts)]
+pub struct SaveUserAuth<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init_if_needed,
+        payer = signer,
+        space = SaveUserAuth::LEN,
+        seeds = ["user-auth".as_bytes(), signer.key().as_ref()], 
+        bump,
+    )]
+    pub user_auth: Account<'info, UserAuth>,
+    pub system_program: Program<'info, System>,
+}
+
+impl SaveUserAuth<'_> {
+    const LEN: usize = 8 + // discriminator
+        32 + // app_id
+        32 + // authority
+        32 + // app_name
+        32 + // auth_uri
+        1; // bump
+}
+
+#[account]
+pub struct AppAuth {
+    pub app_id: Pubkey, // app pubkey
+    pub app_name: String, // app name like "twitter"
+    pub auth_uri: String, // ipfs uri
+    pub bump: u8,
+}
+
+#[account]
+pub struct UserAuth {
+    pub app_id: Pubkey, // app pubkey
+    pub authority: Pubkey, // user pubkey
+    pub app_name: String, // app name like "twitter"
+    pub auth_uri: String, // ipfs uri
+    pub bump: u8,
+}
--- contract/tests/contract.ts
+++ contract/tests/contract.ts
@@ -3,14 +3,72 @@ import { Program } from "@coral-xyz/anchor";
 import { Contract } from "../target/types/contract";
 
 describe("contract", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
-
   const program = anchor.workspace.Contract as Program<Contract>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  // App Auth
+
+  it("Save app auth", async () => {
+    const ix = program.methods.saveAppAuth("X", "app_ipfs_uri");
+    const pdaAddress = (await ix.pubkeys()).appAuth;
+    console.log("PDA address :: ", pdaAddress.toString());
+
+    const tx = await ix.rpc()
     console.log("Your transaction signature", tx);
+
+    let authDetails = await program.account.appAuth.fetch(pdaAddress);
+    console.log("Auth details", JSON.stringify(authDetails, null, 2));
   });
+
+  it("Get already saved app auth", async () => {
+    const appAddress = new anchor.web3.PublicKey("CFkpmQ1mWkxDapUiNUA3uwNG2zjwkTGUEf1Cwh58YZj6");
+    const [pdaAddress, _] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        anchor.utils.bytes.utf8.encode('app-auth'),
+        appAddress.toBuffer(),
+      ],
+      program.programId
+    )
+    try {
+      let authDetails = await program.account.appAuth.fetch(pdaAddress);
+      console.log("Auth details", JSON.stringify(authDetails, null, 2));
+    } catch (error) {
+      console.log("Error fetching the auth :: ", error);
+    }
+  });
+
+  // User Auth
+
+  it("Save user auth", async () => {
+    const appAddress = new anchor.web3.PublicKey("CFkpmQ1mWkxDapUiNUA3uwNG2zjwkTGUEf1Cwh58YZj6");
+
+    const ix = program.methods.saveUserAuth(appAddress, "X", "user_ipfs_uri");
+    const pdaAddress = (await ix.pubkeys()).userAuth;
+    console.log("PDA address :: ", pdaAddress.toString());
+
+    const tx = await ix.rpc()
+    console.log("Your transaction signature", tx);
+
+    let authDetails = await program.account.userAuth.fetch(pdaAddress);
+    console.log("Auth details", JSON.stringify(authDetails, null, 2));
+  });
+
+  it("Get already saved user auth", async () => {
+    const userAddress = new anchor.web3.PublicKey("CFkpmQ1mWkxDapUiNUA3uwNG2zjwkTGUEf1Cwh58YZj6");
+
+    const [pdaAddress, _] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        anchor.utils.bytes.utf8.encode('user-auth'),
+        userAddress.toBuffer(),
+      ],
+      program.programId
+    )
+    try {
+      let authDetails = await program.account.userAuth.fetch(pdaAddress);
+      console.log("Auth details", JSON.stringify(authDetails, null, 2));
+    } catch (error) {
+      console.log("Error fetching the auth :: ", error);
+    }
+  });
+
 });
