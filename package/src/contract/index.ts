1bf5e53fdc5c883279ffac130907df66c9602af1
thatsmeadarsh <thatsmeadarshgupta@gmail.com>
2024-12-21 00:19:20 +0530

    feat: Added save functions for both app and user auth details via smart contract in Contract class

--- package/src/contract/index.ts
+++ package/src/contract/index.ts
@@ -3,6 +3,7 @@ import { Program } from "@coral-xyz/anchor";
 import type { Contract as IDL } from "./idl";
 import { clusterApiUrl, Connection, PublicKey, type Keypair } from "@solana/web3.js";
 import { getKeypair } from "../sdk/helpers/getPublicKey";
+import logger from "../sdk/utils/logger";
 
 
 export class Contract {
@@ -26,6 +27,15 @@ export class Contract {
             }
         )
     }
+
+    async saveAppAuth(appName: string, authURI: string) {
+        await this.program.methods.saveAppAuth(appName, authURI).rpc();
+    }
+
+    async saveUserAuth(appId: PublicKey, appName: string, authURI: string) {
+        await this.program.methods.saveUserAuth(appId, appName, authURI).rpc();
+    }
+
 }
 
 
