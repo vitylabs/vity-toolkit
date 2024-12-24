69c91c7cb674d2ab1643a1631b7603ce5acfb096
thatsmeadarsh <thatsmeadarshgupta@gmail.com>
2024-12-25 02:43:07 +0530

    v0.1.5

--- code-samples/earn/execute/fetch-user-pow.ts
+++ code-samples/earn/execute/fetch-user-pow.ts
@@ -2,6 +2,14 @@ import { VityToolKit, Action } from "vity-toolkit";
 
 
 const toolKit = new VityToolKit();
-const result = await toolKit.executeAction({ action: Action.EARN_FETCH_USER_POWS, inputParams: { userId: '1ce11bfa-7fca-4d0f-9d19-c114b18a0207' } });
+
+// 1. Get expected params for an action
+const expectedParams = toolKit.getInputParamsForAction({ action: Action.EARN_FETCH_USER_POWS });
+console.log(expectedParams);
+
+// 2. Get expected params for executing the action
+const params = { userId: '1ce11bfa-7fca-4d0f-9d19-c114b18a0207' };
+
+const result = await toolKit.executeAction({ action: Action.EARN_FETCH_USER_POWS, inputParams: params });
 
 console.log(result.data);
